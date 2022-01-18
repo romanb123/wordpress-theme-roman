9722f07fce74cec8b69f57a5f770b078a1aef334
romanb123 <romanb123@users.noreply.github.com>
2022-01-18 20:56:40 +0200

    comments-template

--- single.php
+++ single.php
@@ -189,202 +189,38 @@
 
                         <h3 id="comments-title"><span>3</span> Comments</h3>
 
-                        <!-- Comments List
-                                ============================================= -->
-                        <ol class="commentlist clearfix">
+                        <?php
+                                if (comments_open() || get_comments_number()) {
+                                    comments_template();
+                                }
+                                ?>
 
-                            <li class="comment even thread-even depth-1" id="li-comment-1">
 
-                                <div id="comment-1" class="comment-wrap clearfix">
 
-                                    <div class="comment-meta">
+                    </div>
 
-                                        <div class="comment-author vcard">
-
-                                            <span class="comment-avatar clearfix">
-                                                <img src='http://0.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=60'
-                                                    class='avatar avatar-60 photo avatar-default' height='60'
-                                                    width='60' />
-                                            </span>
-
-                                        </div>
-
-                                    </div>
-
-                                    <div class="comment-content clearfix">
-
-                                        <div class="comment-author">
-                                            John Doe
-                                            <span>April 24, 2012 at 10:46 am</span>
-                                        </div>
-
-                                        <p>
-                                            Donec sed odio dui. Nulla vitae elit libero,
-                                            a pharetra augue. Nullam id dolor id nibh
-                                            ultricies vehicula ut id elit. Integer
-                                            posuere erat a ante venenatis dapibus
-                                            posuere velit aliquet.
-                                        </p>
-
-                                    </div>
-
-                                    <div class="clear"></div>
-
-                                </div>
-
-                            </li>
-
-                            <li class="comment even thread-even depth-1" id="li-comment-2">
-
-                                <div id="comment-2" class="comment-wrap clearfix">
-
-                                    <div class="comment-meta">
-
-                                        <div class="comment-author vcard">
-
-                                            <span class="comment-avatar clearfix">
-                                                <img src='http://0.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=60'
-                                                    class='avatar avatar-60 photo avatar-default' height='60'
-                                                    width='60' />
-                                            </span>
-
-                                        </div>
-
-                                    </div>
-
-                                    <div class="comment-content clearfix">
-
-                                        <div class="comment-author">
-                                            John Doe
-                                            <span>April 24, 2012 at 10:46 am</span>
-                                        </div>
-
-                                        <p>
-                                            Donec sed odio dui. Nulla vitae elit libero,
-                                            a pharetra augue. Nullam id dolor id nibh
-                                            ultricies vehicula ut id elit. Integer
-                                            posuere erat a ante venenatis dapibus
-                                            posuere velit aliquet.
-                                        </p>
-
-                                    </div>
-
-                                    <div class="clear"></div>
-
-                                </div>
-
-                            </li>
-
-                            <li class="comment even thread-even depth-1" id="li-comment-3">
-
-                                <div id="comment-3" class="comment-wrap clearfix">
-
-                                    <div class="comment-meta">
-
-                                        <div class="comment-author vcard">
-
-                                            <span class="comment-avatar clearfix">
-                                                <img src='http://0.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=60'
-                                                    class='avatar avatar-60 photo avatar-default' height='60'
-                                                    width='60' />
-                                            </span>
-
-                                        </div>
-
-                                    </div>
-
-                                    <div class="comment-content clearfix">
-
-                                        <div class="comment-author">
-                                            John Doe
-                                            <span>April 24, 2012 at 10:46 am</span>
-                                        </div>
-
-                                        <p>
-                                            Donec sed odio dui. Nulla vitae elit libero,
-                                            a pharetra augue. Nullam id dolor id nibh
-                                            ultricies vehicula ut id elit. Integer
-                                            posuere erat a ante venenatis dapibus
-                                            posuere velit aliquet.
-                                        </p>
-
-                                    </div>
-
-                                    <div class="clear"></div>
-
-                                </div>
-
-                            </li>
-
-                        </ol><!-- .commentlist end -->
-
-                        <div class="clear"></div>
-
-                        <!-- Comment Form
-                                ============================================= -->
-                        <div id="respond" class="clearfix">
-
-                            <h3>Leave a <span>Comment</span></h3>
-
-                            <form class="clearfix" action="#" method="post" id="commentform">
-
-                                <div class="col_one_third">
-                                    <label>Name</label>
-                                    <input type="text" name="author" class="sm-form-control" />
-                                </div>
-
-                                <div class="col_one_third">
-                                    <label>Email</label>
-                                    <input type="text" name="email" class="sm-form-control" />
-                                </div>
-
-                                <div class="col_one_third col_last">
-                                    <label>Website</label>
-                                    <input type="text" name="url" class="sm-form-control" />
-                                </div>
-
-                                <div class="clear"></div>
-
-                                <div class="col_full">
-                                    <label>Comment</label>
-                                    <textarea name="comment" cols="58" rows="7" class="sm-form-control"></textarea>
-                                </div>
-
-                                <div class="col_full nobottommargin">
-                                    <button type="submit" id="submit-button" class="button button-3d nomargin">Submit
-                                        Comment</button>
-                                </div>
-
-                            </form>
-
-                        </div><!-- #respond end -->
-
-                    </div><!-- #comments end -->
-
-                </div>
-
-                <?php
+                    <?php
                     }
                 }
-                ?>
+                    ?>
 
-                <div class="sidebar nobottommargin col_last">
-                    <div class="sidebar-widgets-wrap">
+                    <div class="sidebar nobottommargin col_last">
+                        <div class="sidebar-widgets-wrap">
 
-                        <?php
-                        if (is_active_sidebar('ju_sidebar-1')) {
-                            dynamic_sidebar('ju_sidebar-1');
-                        }
+                            <?php
+                            if (is_active_sidebar('ju_sidebar-1')) {
+                                dynamic_sidebar('ju_sidebar-1');
+                            }
 
-                        ?>
+                            ?>
+                        </div>
                     </div>
-                </div>
 
-            </div><!-- .sidebar end -->
+                </div><!-- .sidebar end -->
+
+            </div>
 
         </div>
-
-    </div>
 
 </section><!-- #content end -->
 
